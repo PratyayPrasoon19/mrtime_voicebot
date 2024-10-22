b565e155484b650b1c92c02b4c47446483cd80dd
Pratyay Prasoon <154357898+PratyayPrasoon19@users.noreply.github.com>
2024-10-22 11:17:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ load_dotenv()
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return 'index.html'
 
 prompt = """You are an AI Voice Bot, trained on multiple prospects and have to give a proper reply to the user based on user_query.
             You have to consider chat_history before generating result.
